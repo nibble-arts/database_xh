9bdac9935410ff26bbbad8e04196c036a75ca6c2
Thomas Winkler <thomas.winkler@tmw.ac.at>
2018-03-26 10:19:26 +0200

    Signed-off-by: Thomas Winkler <thomas.winkler@tmw.ac.at>

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
 $plugin_cf['database']['database_server']="localhost";
-$plugin_cf['database']['database_user']="w1142";
-$plugin_cf['database']['database_password']="Favd@114";
-$plugin_cf['database']['database_name']="w1142_voefa";
+$plugin_cf['database']['database_user']="username";
+$plugin_cf['database']['database_password']="password";
+$plugin_cf['database']['database_name']="database name";
 
 $plugin_cf['database']['path_queries']="queries/";
 $plugin_cf['database']['path_templates']="templates/";
